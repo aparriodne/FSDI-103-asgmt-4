d7ae724eb64662cbb98aff95820c20df1952b4f1
aparriodne <121842938+aparriodne@users.noreply.github.com>
2023-02-01 00:11:33 -0800

    FSDI-103-asgmt-4

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # FSDI-103-asgmt-4
+Create editable to-do list. 
---
+++ script/todo.js
@@ -0,0 +1,9 @@
+let board = [];
+let entryID = 0;
+function addTask(){
+    let entry = document.getElementById("txtTask").value;
+    board.push(entry);
+    
+}
+
+function buyItem(){}
